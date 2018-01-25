fe35aafb43c5662ff232d179d2db3db4811a06d2
CarlosFernando <ca_fer89@hotmail.com>
2018-01-25 09:16:01 -0500

    bottom bar added

--- app/src/main/java/com/chemicalgorithm/platzigram/view/LoginActivity.java
+++ app/src/main/java/com/chemicalgorithm/platzigram/view/LoginActivity.java
@@ -4,17 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.chemicalgorithm.platzigram.R;
 
 public class LoginActivity extends AppCompatActivity
 {
 
+	Button btn_login;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		btn_login = (Button) findViewById(R.id.login);
+		btn_login.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent i = new Intent(LoginActivity.this, ContainerActivity.class);
+				startActivity(i);
+			}
+		});
 	}
 
 	public void goCreateAccount(View view)
