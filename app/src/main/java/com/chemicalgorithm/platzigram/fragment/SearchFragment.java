aae4ef3aafa60f7f37ac9d03f0da95cdb184b7af
CarlosFernando <ca_fer89@hotmail.com>
2018-02-12 19:04:24 -0500

    building fragmentSearch.Java

--- app/src/main/java/com/chemicalgorithm/platzigram/fragment/SearchFragment.java
+++ app/src/main/java/com/chemicalgorithm/platzigram/fragment/SearchFragment.java
@@ -2,12 +2,15 @@ package com.chemicalgorithm.platzigram.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
 import com.chemicalgorithm.platzigram.R;
+import com.chemicalgorithm.platzigram.adapter.PictureAdapterRecyclerView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +32,10 @@ public class SearchFragment extends Fragment
 		View view = inflater.inflate(R.layout.fragment_search, container, false);
 		SearchView searchView = (SearchView) view.findViewById(R.id.search_view);
 		searchView.setSubmitButtonEnabled(true);
+
+		RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.search_recycler);
+		GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), GridLayoutManager.DEFAULT_SPAN_COUNT);
+		recyclerView.setLayoutManager(gridLayoutManager);
 		return view;
 
 	}
