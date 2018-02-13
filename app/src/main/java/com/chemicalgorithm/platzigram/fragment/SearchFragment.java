a38c5ed0a08cf20d0eed6d35a0616817a0732179
CarlosFernando <ca_fer89@hotmail.com>
2018-02-12 19:15:52 -0500

    Grid added, works good

--- app/src/main/java/com/chemicalgorithm/platzigram/fragment/SearchFragment.java
+++ app/src/main/java/com/chemicalgorithm/platzigram/fragment/SearchFragment.java
@@ -11,6 +11,9 @@ import android.widget.SearchView;
 
 import com.chemicalgorithm.platzigram.R;
 import com.chemicalgorithm.platzigram.adapter.PictureAdapterRecyclerView;
+import com.chemicalgorithm.platzigram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,10 +37,28 @@ public class SearchFragment extends Fragment
 		searchView.setSubmitButtonEnabled(true);
 
 		RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.search_recycler);
-		GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), GridLayoutManager.DEFAULT_SPAN_COUNT);
+
+		GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),2);
 		recyclerView.setLayoutManager(gridLayoutManager);
+
+		PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture, getActivity());
+		recyclerView.setAdapter(pictureAdapterRecyclerView);
 		return view;
 
+	}
+
+	public ArrayList<Picture> buildPictures()
+	{
+		ArrayList<Picture> pictures = new ArrayList<>();
+		pictures.add(new Picture("https://pbs.twimg.com/profile_images/953823390695088129/3HlXltqB_400x400.jpg",
+				"Danielita hermosa", "4 días", "3 me gusta"));
+		pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg",
+				"Juan Pablo", "3 días", "10 me gusta"));
+		pictures.add(new Picture("https://i.pinimg.com/564x/fd/91/9a/fd919ae49b855848f2c407f363d44aef--gato-cheshire-the-cheshire.jpg",
+				"Carlos Pinzón", "2 días", "20 me gusta"));
+		pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg",
+				"Anahí Salgado", "3 días", "0 me gusta"));
+		return pictures;
 	}
 
 }
