c871f9904acb455e7fd61a05e6688bb78b43b198
CarlosFernando <ca_fer89@hotmail.com>
2018-02-20 10:21:31 -0500

    vertical NavigationBottom test - failed

--- app/src/main/java/com/chemicalgorithm/platzigram/LoginActivity.java
+++ app/src/main/java/com/chemicalgorithm/platzigram/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.chemicalgorithm.platzigram;
 
+import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.chemicalgorithm.platzigram.view.ContainerActivity;
 import com.chemicalgorithm.platzigram.view.CreateAccountActivity;
@@ -13,12 +16,24 @@ public class LoginActivity extends AppCompatActivity
 {
 
 	Button btn_login;
+	ImageView img_logo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		img_logo = (ImageView) findViewById(R.id.logo);
+		img_logo.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				webSearch("https://github.com/cafer89/Platzigram");
+			}
+		});
+
 
 		btn_login = (Button) findViewById(R.id.login);
 		btn_login.setOnClickListener(new View.OnClickListener()
@@ -30,6 +45,8 @@ public class LoginActivity extends AppCompatActivity
 				startActivity(i);
 			}
 		});
+
+
 	}
 
 	public void goCreateAccount(View view)
@@ -38,5 +55,16 @@ public class LoginActivity extends AppCompatActivity
 		Intent intent = new Intent(LoginActivity.this,
 				CreateAccountActivity.class);
 		startActivity(intent);
+	}
+
+	public void webSearch(String link)
+	{
+		Uri  webPage = Uri.parse(link);
+		Intent webIntent = new Intent(Intent.ACTION_VIEW, webPage);
+
+		if(webIntent.resolveActivity(getPackageManager()) !=null)
+		{
+			startActivity(webIntent);
+		}
 	}
 }
