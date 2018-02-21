d3269db91a80dad0dd2d92b45e898893ed7cda4d
CarlosFernando <ca_fer89@hotmail.com>
2018-02-21 11:00:07 -0500

    Camera feature added

--- app/src/main/java/com/chemicalgorithm/platzigram/post/view/HomeFragment.java
+++ app/src/main/java/com/chemicalgorithm/platzigram/post/view/HomeFragment.java
@@ -1,12 +1,16 @@
-package com.chemicalgorithm.platzigram.fragment;
+package com.chemicalgorithm.platzigram.post.view;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,13 +26,13 @@ import java.util.ArrayList;
  */
 public class HomeFragment extends Fragment
 {
-
+	private final int REQUEST_CAMERA = 1;
+	private FloatingActionButton fabCamera;
 
 	public HomeFragment()
 	{
 		// Required empty public constructor
 	}
-
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +41,7 @@ public class HomeFragment extends Fragment
 		View view = inflater.inflate(R.layout.fragment_home, container, false);
 		showToolbar(getResources().getString(R.string.bottombar_home), false, view);
 		RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+		fabCamera = (FloatingActionButton) view.findViewById(R.id.fabCamera);
 
 		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
 		linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -45,8 +50,26 @@ public class HomeFragment extends Fragment
 		PictureAdapterRecyclerView pictureAdapterRecyclerView =
 				new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture, getActivity());
 		picturesRecycler.setAdapter(pictureAdapterRecyclerView);
-		return view;
 
+		fabCamera.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				takePicture();
+			}
+		});
+		return view;
+	}
+
+	private void takePicture()
+	{
+		Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if(intentTakePicture.resolveActivity(getActivity().getPackageManager()) != null)
+		{
+			startActivityForResult(intentTakePicture, REQUEST_CAMERA);
+
+		}
 	}
 
 	public ArrayList<Picture> buildPictures()
@@ -63,10 +86,6 @@ public class HomeFragment extends Fragment
 		pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg",
 				"Anahí Salgado", "3 días", "0 me gusta"));
 		return pictures;
-
-
-
-
 	}
 
 	public void showToolbar(String title , boolean upButton, View view)
@@ -77,5 +96,15 @@ public class HomeFragment extends Fragment
 		((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
 		//en caso de que tenga botón de regreso o subir
 		((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == REQUEST_CAMERA && resultCode == getActivity().RESULT_OK)
+		{
+			Log.d("Home Fragment", "CAMERA OK !");
+		}
 	}
 }
